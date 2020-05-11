916d91fce883de286ba2ffec1e4cab0bce3c70b3
Miha Moskon <miha.moskon@gmail.com>
2020-05-11 23:12:09 +0200

    Updates from Overleaf

---
+++ resitve/beri_csv_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+A1 = np.genfromtxt("stevila1.csv", encoding="utf-8", delimiter=",")
+B1 = np.loadtxt("stevila1.csv", encoding="utf-8", delimiter=",")
+
+A2 = np.genfromtxt("stevila2.csv", encoding="utf-8", delimiter=",")
+B2 = np.loadtxt("stevila2.csv", encoding="utf-8", delimiter=",", dtype='str')
+
+A3 = np.genfromtxt("stevila3.csv", encoding="utf-8", delimiter=",", names=True)
+B3 = np.loadtxt("stevila3.csv", encoding="utf-8", delimiter=",", dtype='U', skiprows=0)
+
+
